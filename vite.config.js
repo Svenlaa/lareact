e3025dd3c70f2cd028666a0d49ecb02369a754d9
Svenlaa <mail@svenlaa.com>
2025-05-09 11:29:52 +0200

    use ddev for development, assume .test domain

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
-            ssr: 'resources/js/ssr.tsx',
+            input: "resources/js/app.tsx",
+            ssr: "resources/js/ssr.tsx",
             refresh: true,
         }),
         react(),
     ],
+    server: {
+        host: "0.0.0.0",
+        origin: `${process.env.DDEV_PRIMARY_URL.replace(/:\d+$/, "")}:5173`,
+        cors: {
+            origin: /https?:\/\/([A-Za-z0-9\-\.]+)?(\.test)(?::\d+)?$/,
+        },
+    },
 });
